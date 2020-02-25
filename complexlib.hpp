6f256150918ff41a9544641efae88c992c79f214
izzybogart <60237226+izzybogart@users.noreply.github.com>
2020-02-25 14:30:54 -0700

    function prototypes for complex number arithmatic

---
+++ complexlib.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Complex {
+	private:
+		double _real;
+		double _imag;
+	public:
+		//constructors and destructors
+		Complex();
+		Complex(double real, double imag);
+		Complex(const Complex &c1);
+		~Complex();
+		
+		//functions
+		double real() const;
+		double imag() const;
+		void print();
+		
+		Complex add(Complex c1);
+		Complex sub(Complex c1);
+		Complex mult(Complex c1);
+		Complex div(Complex c1);
+
+		double magnitude();
+		double phase();
+		
+		//overload
+		Complex operator+(Complex c1);
+		Complex operator-(Complex c1);
+		Complex operator*(Complex c1);
+		Complex operator/(Complex c1);
+		Complex operator=(Complex c1);
+
+		friend std::ostream& operator<<(std::ostream &out, const Complex &c1);
+		friend std::istream& operator>>(std::istream &in, Complex &c1);
+};
+
